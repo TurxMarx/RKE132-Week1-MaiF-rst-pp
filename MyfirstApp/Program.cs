661468d663a3045a0e51e2ee51bc0790e52938c8
unknown <margus.turgan@tktk.ee>
2024-02-18 13:47:36 +0200

    create new app

---
+++ MyfirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" +", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
